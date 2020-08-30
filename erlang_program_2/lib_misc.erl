263bce70b08b4c91b0fb1e31427d1db24ed73026
sunshuai <svensama@outlook.com>
2020-08-30 23:30:21 +0800

    some logic

--- erlang_program_2/lib_misc.erl
+++ erlang_program_2/lib_misc.erl
@@ -1,5 +1,5 @@
 -module (lib_misc).
--export ([for/3, quicksort/1, pythag/1]).
+-export ([for/3, quicksort/1, pythag/1, perms/1, odds_and_evens1/1, odds_and_evens2/1]).
 
 for(Max, Max, F) -> [F(Max)];
 
@@ -20,3 +20,24 @@ pythag(N) ->
 		A+B+C =< N,
 		A*A+B*B =:= C*C
 	].
+
+perms([]) -> [[]];
+perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
+
+odds_and_evens1(L) ->
+	Odds  = [X||X<-L, (X rem 2) =:=0],
+	Evens = [X||X<-L, (X rem 2) =:=1],
+	{Odds, Evens}.
+
+odds_and_evens2(L) ->
+	odds_and_evens_cal(L, [], []).
+
+odds_and_evens_cal([H|T], Odds, Evens) ->
+	if
+	 (H rem 2) =:= 0 -> odds_and_evens_cal(T, [H|Odds], Evens);
+	 (H rem 2) =:= 1 -> odds_and_evens_cal(T, Odds, [H|Evens])
+	end;
+
+odds_and_evens_cal([], Odds, Evens) ->
+	{Odds, Evens}.
+	
