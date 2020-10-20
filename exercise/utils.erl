0c76da9c23ac497901eb138e4736ef087c125306
sunshuai <svensama@outlook.com>
2020-10-21 00:30:23 +0800

    lists:sort(Fun, List).

--- exercise/utils.erl
+++ exercise/utils.erl
@@ -1,22 +1,20 @@
 -module (utils).
--export ([generate_pokers/0, pokers_sort/2, get_pokers_type/1, compare/2]).
+-export ([generate_pokers/0, pokers_sort/2, get_pokers_type/1, compare2B/2, compare2S/2]).
 -include ("records.hrl").
-
-compare([#poker{} = A], [#poker{} = B]) ->
+% lists:sort(fun utils:compare/2, Pokers).
+compare2B(#poker{} = A, #poker{} = B) ->
 	if
 		A#poker.value > B#poker.value -> true;
+		true -> false
+	end.
+compare2S(#poker{} = A, #poker{} = B) ->
+	if
+		A#poker.value < B#poker.value -> true;
 		true -> false
 	end.
 
 generate_pokers() ->
 	lists:seq(1,54).
-
-pokers_sort(s2b, L) ->
-	lists:keysort(L, 2);
-
-pokers_sort(b2s, L) ->
-	lists:reverse(lists:keysort(L, 2)).
-
 
 get_pokers_type(_L) ->
 	Type = #poker_type{},
